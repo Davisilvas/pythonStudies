3cd7cbdda11f0d1d18242802915bf2007e776638
Davi Silva Santos <morg.daviss@gmail.com>
2022-05-25 20:06:39 -0300

    reorganizei a pasta

---
+++ sectionOneBasicPython/firstSteps/basicLevel/lesson4/lesson4.py
@@ -0,0 +1,22 @@
+'''
+str - string
+int - inteiro
+float - real/ponto flutuante
+bool - boolean
+'''
+
+print('Isso é uma', type('Davi'))
+print(10, type(10))
+print(10.3, type(10.3))
+print(10==10, type(10==10))
+print('l' == 'l', type('l' == 'l'))
+
+print('10', type('10'), int('10'))
+
+print(10 + 10)
+
+# exercício da aula
+print('Davi', type('Davi'))
+print(21, type(21))
+print(1.85, type(1.85))
+print(21 > 18, type(21 > 18))
